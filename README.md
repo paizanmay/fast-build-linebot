a9d5bc47c81dc4458c6f563eab26aa1c2ea44b7c
霈政 邱 <paizanmay@gmail.com>
2018-10-06 01:00:48 +0800

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # fast-build-linebot
+這是可以讓你快速 build 並 deploy 到 heroku 的一個 Line Chatbot 的 repo，只要跟著下面的步驟做，一下就可以完成了
+
+# How to Install
+參考 [Heroku網站](https://devcenter.heroku.com/articles/getting-started-with-python)的教學
+首先要完成下面4件事
+* 申請 heroku 帳號並安裝 heroku CLI (`brew install heroku/brew/heroku`)
+* 在本機端有安裝好 Python3
+* 安裝 pipenv (```pip install pipenv```)
+* 安裝 Postgres
+  
+接著按照下面的步驟  
+首先先將這個 repo clone 回來
+```bash
+git clone https://github.com/paizanmay/fast-build-linebot
+cd fast-build-linebot
+```
+接著設定 heroku
+```bash
+heroku login
+heroku create
+```
+然後安裝 python packages
+```bash
+pipenv install
+```
+
---
+++ flask_test.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi("raJIOE2LaacIFk0uUKcF10plUSqcQUJdyQLryRe1X8FFs7dzhrj2vx14vWCn7t9SlGvYPrMtbxefCbQnLhsms5VJASBSr64xeRhf9ixXT3f0aFzQL6asptBQFCIMICNd3RgajKq/m0GKAE5ImXJJaAdB04t89/1O/w1cDnyilFU=Issue")
+handler = WebhookHandler('cf00401a45cbd9fd2b366408f4759cb3')
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__":
+    app.run()
